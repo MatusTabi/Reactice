5f23684374cd5a004fe599c83fc799a45f3e5edb
xtabi <xtabi@fi.muni.cz>
2026-05-07 15:19:27 +0200

    feat: add header component

---
+++ src/components/dashboard/code/script-area/ScriptAreaHeader.tsx
@@ -0,0 +1,14 @@
+import { PropsWithChildren } from 'react';
+
+type ScriptAreaHeaderProps = PropsWithChildren & {
+	title: string;
+};
+
+const ScriptAreaHeader = ({ children, title }: ScriptAreaHeaderProps) => (
+	<header className="bg-surface-container-heighest border-outline flex h-10 w-full items-center justify-between border p-2">
+		<h2 className="text-on-surface">{title}</h2>
+		{children}
+	</header>
+);
+
+export default ScriptAreaHeader;
