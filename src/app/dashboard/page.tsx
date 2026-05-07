e6d50f8c01f0146a66426ef44d28e820e7bf81a4
xtabi <xtabi@fi.muni.cz>
2026-05-07 15:18:39 +0200

    feat: adjust padding

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import CodeEditor from '@/components/dashboard/code/CodeEditor';
 
 const LearningDashboard = () => (
-	<main className="bg-surface h-screen px-16 py-24">
+	<main className="bg-surface h-screen pt-24">
 		<CodeEditor />
 	</main>
 );
