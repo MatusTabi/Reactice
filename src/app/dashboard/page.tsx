095e7a08292be80b9caf9078f694bc603f0834ff
xtabi <xtabi@fi.muni.cz>
2026-05-07 09:07:05 +0200

    feat: create dashboard component with code editor

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import CodeEditor from '@/components/dashboard/code/CodeEditor';
+
+const LearningDashboard = () => (
+	<main className="bg-surface h-screen px-16 py-24">
+		<CodeEditor />
+	</main>
+);
+
+export default LearningDashboard;
