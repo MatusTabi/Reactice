71fd2b15deac4c59e2e3f97132c055d9c3e273e6
xtabi <xtabi@fi.muni.cz>
2026-05-11 10:17:57 +0200

    fix: adjust dashboard main height

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import CodeEditor from '@/components/dashboard/code/CodeEditor';
 
 const LearningDashboard = () => (
-	<main className="bg-surface h-screen w-full">
+	<main className="bg-surface h-[calc(100vh-69px)] w-full">
 		<CodeEditor />
 	</main>
 );
