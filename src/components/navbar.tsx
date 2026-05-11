781f33996067706d959ebc9e864f572b4d343b55
MatusTabi <matustabi44@gmail.com>
2026-05-11 16:31:35 +0200

    feat: make navbar sticky on top

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,7 +7,7 @@ export const Navbar = async () => {
 	const session = await auth();
 
 	return (
-		<nav className="border-b border-gray-200 bg-white">
+		<nav className="sticky top-0 z-10 border-b border-gray-200 bg-white">
 			<div className="mx-auto flex max-w-6xl items-center justify-between px-6 py-4">
 				<Link href="/" className="text-xl font-bold text-gray-900">
 					Reactice
