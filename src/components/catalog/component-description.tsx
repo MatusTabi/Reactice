422fc11effa3d09cb1bb1a27c66c476cea861213
MatusTabi <matustabi44@gmail.com>
2026-05-11 22:42:07 +0200

    feat: add line clamping to component description

--- src/components/catalog/component-description.tsx
+++ src/components/catalog/component-description.tsx
@@ -1,5 +1,5 @@
 const ComponentDescription = ({ description }: { description: string }) => (
-	<p className="text-foreground mt-4 text-sm">{description}</p>
+	<p className="text-foreground mt-4 line-clamp-3 text-sm">{description}</p>
 );
 
 export default ComponentDescription;
