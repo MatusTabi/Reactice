74bc4eb0709033bd8d9a3fc000598568b2331663
MatusTabi <matustabi44@gmail.com>
2026-05-12 12:25:44 +0200

    feat: update script area styles for improved visual consistency

--- src/components/dashboard/code/script-area/ScriptAreaHeader.tsx
+++ src/components/dashboard/code/script-area/ScriptAreaHeader.tsx
@@ -5,7 +5,9 @@ type ScriptAreaHeaderProps = PropsWithChildren & {
 };
 
 const ScriptAreaHeader = ({ children, title }: ScriptAreaHeaderProps) => (
-	<header className="bg-surface-container-heighest border-outline flex h-10 w-full items-center justify-between border px-4 py-2">
+	<header
+		className={`bg-background text-foreground border-accent flex h-10 w-full items-center justify-between border px-4 py-2`}
+	>
 		<h2 className="text-on-surface">{title}</h2>
 		{children}
 	</header>
